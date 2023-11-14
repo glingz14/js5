715b30135ba2318f7728ea9b5c62e78ee2c4c1ce
glingz14 <125362709+glingz14@users.noreply.github.com>
2023-11-14 13:38:37 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const emptyArray = [];
+
+const numbers = [1,2,3];
+
+const fruits = ['клубника','банан','яблоко'];
+
+console.log (fruits[0]);
+console.log (fruits[1]);
+
+
+
+for (let i=0;i<fruits.length;i++)[
+ console.log (fruits[i])   
+]
+
+const myclass1 = document.querySelector('.myclass1').textContent
+console.log(myclass1)
+
+let tima = []
+console.log (tima)
+tima.push(myclass1)
+console.log (tima)
+
+
+const myclass2 = document.querySelector('.myclass2x').textContent
+console.log(myclass1)
+
+let miras = []
+console.log (miras)
+tima.push(myclass2)
+console.log (miras)
+
+const myclass3 = document.querySelector('.myclass3').textContent
+console.log(myclass3)
+
+let rafaelka = []
+console.log (rafaelka)
+tima.push(myclass1)
+console.log (rafaelka)
+ 
+
+
